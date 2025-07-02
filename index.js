fdc5a25792cc9a67ef878b38427e3a68c9a7b0cc
Jayasri P <jayaporalla921@gmail.com>
2025-07-02 19:34:39 +0530

    HTTP Methods

---
+++ index.js
@@ -0,0 +1,151 @@
+const http = require("http");
+let users = [
+  {
+    id: 1,
+    name: "Jayasri",
+  },
+  {
+    id: 2,
+    name: "Saiteja",
+  },
+  {
+    id: 3,
+    name: "Chinni",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(
+      JSON.stringify({
+        users,
+      })
+    );
+    res.end();
+  } else if (req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      try {
+        data = JSON.parse(data);
+        console.log(data);
+
+        if (!data || !data.name) {
+          res.statusCode = 422;
+          res.setHeader("Content-Type", "application/json");
+          res.write(
+            JSON.stringify({
+              message: "Invalid Data",
+            })
+          );
+          res.end();
+        }
+        const user = {
+          id: Date.now(),
+          name: data.name,
+        };
+        users.push(user);
+        res.statusCode = 201; //created
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            user
+          })
+        );
+        res.end();
+      } catch (error) {
+        res.statusCode = 500; //created
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            message: "Something went wrong, try again later!",
+          })
+        );
+        res.end();
+      }
+    });
+  } else if (req.method === "PUT") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      try {
+        data = JSON.parse(data);
+        if (!data || !data.name || !data.id) {
+          res.statusCode = 422;
+          res.setHeader("Content-Type", "application/json");
+          res.write(
+            JSON.stringify({
+              message: "Invalid Data"
+            })
+          );
+          res.end();
+        }
+        const user = users.find((u) => u.id === data.id);
+        user.name = data.name;
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            user
+          })
+        );
+        res.end();
+      } catch (error) {
+        res.statusCode = 500; // Server Error
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            message: "Something went wrong, try again later!",
+          })
+        );
+        res.end();
+      }
+    });
+  } else if (req.method === "DELETE") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      try {
+        data = JSON.parse(data);
+        if (!data || !data.id) {
+          res.statusCode = 422;
+          res.setHeader("Content-Type", "application/json");
+          res.write(
+            JSON.stringify({
+              message: "Invalid Data"
+            })
+          );
+          res.end();
+        }
+        const userExists = users.some((u) => u.id === data.id);
+        users = users.filter((u) => u.id !== data.id);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            message: "Successfully Deleted"
+          })
+        );
+        res.end();
+      } catch (error) {
+        res.statusCode = 500; // Server Error
+        res.setHeader("Content-Type", "application/json");
+        res.write(
+          JSON.stringify({
+            message: "Something went wrong, try again later!"
+          })
+        );
+        res.end();
+      }
+    });
+  }
+});
+
+server.listen(8080, () => console.log("Server open on 8080"));
---
+++ tcp.js
@@ -0,0 +1,10 @@
+// const net = require("net");
+
+// const server = net.createServer((socket) => {
+//     socket.on("data", (data) => {
+//         console.log("Data received via TCP client: ", data.toString());
+//     });
+//     socket.on('end', () => console.log("TCP Connection ended"));
+// });
+
+// server.listen(8080, () => console.log("TCP Sever opened on 8080"));
